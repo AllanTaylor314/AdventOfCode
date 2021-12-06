0158e0f649f5bb561001e5c857a2a90141a8fbca
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-06 20:17:53 +1300

    2021 Day 6 UPDATE
    Move from using a dict to a list as that will use less memory and prevent the need for an inner loop (just use a couple of list slices and no temp variable). Also remove the initial approach to part 1 since it was kinda slow (and I know this approach works, so I don't need the first one for verification). This may be slightly less readable than the previous version, but it works! (and it's much shorter)

--- 2021/06.py
+++ 2021/06.py
@@ -1,30 +1,8 @@
 with open('06.txt') as file:
-    data=file.read().split(',')
-fish_init=list(map(int,data))
-
-fish=fish_init
-for day in range(80):
-    new_fish=[]
-    for i,f in enumerate(fish):
-        if f==0:
-            new_fish.append(6)
-            new_fish.append(8)
-        else:
-            new_fish.append(f-1)
-    fish=new_fish
-print('Part 1:',len(fish),flush=True)
-
-# Second approach - store the fish in a dict of the number of
-# fish with each value - linear time rather than exponential
-
-fish_counts={i:fish_init.count(i) for i in range(9)}
+    data=list(map(int,file.read().split(',')))
+fish_counts=[data.count(i) for i in range(9)]
 for day in range(256):
-    new_counts={}
-    for i in range(1,9): # Decrement all fish
-        new_counts[i-1]=fish_counts[i]
-    new_counts[8]=fish_counts[0] # Create new fish with time=8
-    new_counts[6]+=fish_counts[0] # 'Decrement' 0 (+= not = for 7->6)
-    fish_counts=new_counts
-    # Check that the original approach for part 1 matches
-    if day==79: print('Part 1:',sum(fish_counts.values()))
-print('Part 2:',sum(fish_counts.values()))
+    fish_counts=fish_counts[1:]+fish_counts[:1]
+    fish_counts[6]+=fish_counts[8]
+    if day==79: print('Part 1:',sum(fish_counts))
+print('Part 2:',sum(fish_counts))
