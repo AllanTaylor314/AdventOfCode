fcdf3222e85f162b66b7d75dd7904eef6f567fa0
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-11 13:13:08 +1300

    2016 Day 17 UPDATE

--- 2016/17.py
+++ 2016/17.py
@@ -1,29 +1,20 @@
-import hashlib
+from hashlib import md5
+####### INPUT #######
 PASSCODE = 'pslxynzg'
-
+#####################
 
 def gen_hash(path):
-    return str(hashlib.md5((PASSCODE + str(path)).encode()).hexdigest())
-
+    return str(md5((PASSCODE + path).encode()).hexdigest())
 
 def next_directions(path):
-    chars = gen_hash(path)[:4]
-    doors = 'UDLR'
-    out = []
-    for i in range(4):
-        if chars[i] > 'a':
-            out.append(path + doors[i])
-    return out
-
+    return [path+door for door,char in zip('UDLR',gen_hash(path)) if char>'a']
 
 def get_position(path):
     """(x,y)"""
     return path.count('D') - path.count('U'), path.count('R') - path.count('L')
 
-
 paths_to_test = ['']
 final_paths=[]
-part1=True
 while paths_to_test:
     path = paths_to_test.pop(0)
     x, y = get_position(path)
@@ -31,9 +22,7 @@ while paths_to_test:
         continue
     if (x, y) == (3, 3):
         final_paths.append(path)
-        if part1:
-            print('Part 1:',path)
-            part1=False
     else:
         paths_to_test.extend(next_directions(path))
+print('Part 1:',final_paths[0])
 print('Part 2:',max(map(len,final_paths)))
