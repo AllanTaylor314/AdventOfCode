18294817c3e53b29cd6c67289a1912f5956be495
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-14 19:10:29 +1300

    2021 Day 14 UPDATE
    defaultdict to avoid zeros (and an assertion to that effect)
    change subs to be a dictionary (formerly sub_dict)

--- 2021/14.py
+++ 2021/14.py
@@ -1,17 +1,14 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 with open('14.txt') as file:
     data = file.read()
 polymer,_,*subs_str=data.splitlines()
-subs=[s.split(' -> ') for s in subs_str]
-sub_dict={a:b for a,b in subs}
+subs={a:b for a,b in (s.split(' -> ') for s in subs_str)}
 
 def polymerise(poly):
     out=""
     for a,b in zip(poly,poly[1:]):
-        out+=a
-        if a+b in sub_dict:
-            out+=sub_dict[a+b]
+        out+=a+subs.get(a+b,'')
     return out+b
 
 poly=polymer
@@ -19,23 +16,22 @@ for _ in range(10):
     poly=polymerise(poly)
 
 c1=Counter(poly)
-print('Part 1:',max(c1.values())-min(c1.values()),flush=True)
+print('Part 1:',max(c1.values())-min(c1.values()))
 
-ALPHABET='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-pair_count={a+b:0 for a in ALPHABET for b in ALPHABET}
+pair_count=defaultdict(int)
 for a,b in zip(polymer,polymer[1:]):
     pair_count[a+b]+=1
 for _ in range(40):
     new_pair_count=pair_count.copy()
-    for pair,new in subs:
+    for pair,new in subs.items():
         count = pair_count[pair]
         new_pair_count[pair]-=count
         a,b=pair
         new_pair_count[a+new]+=count
         new_pair_count[new+b]+=count
     pair_count=new_pair_count
-counter = {a:0 for a in ALPHABET}
-counter2= {b:0 for b in ALPHABET}
+counter = defaultdict(int)
+counter2= defaultdict(int)
 for pair, count in pair_count.items():
     a,b = pair
     counter[a]+=count
@@ -43,6 +39,5 @@ for pair, count in pair_count.items():
 counter2[polymer[0]]+=1
 counter[polymer[-1]]+=1
 assert counter==counter2
-non_zero_counts = set(counter.values())
-non_zero_counts.discard(0)
-print('Part 2:',max(non_zero_counts)-min(non_zero_counts))
+assert 0 not in counter.values()
+print('Part 2:',max(counter.values())-min(counter.values()))
