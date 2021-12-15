4658298714bcfe95815902c026ea122d418e0515
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-15 23:10:14 +1300

    2021 Day 15
    Part 2 is solved by setting the variable PART2 to True on line 26. It is kind of chance that it actually solves it eventually. I'm sure someone could find an input that this doesn't work for, but that could probably be 'solved' by changing the loop on line 40. I assume that there is no need to enter the same space twice as that would always be a worse path.

---
+++ 2021/15.py
@@ -0,0 +1,42 @@
+DXDY=((0,-1),(0,1),(-1,0),(1,0))
+
+class BestWeightToPoint:
+    loc_dict={}
+    def __init__(self,x,y,r):
+        self.x=x
+        self.y=y
+        self.weight=float('inf')
+        self.risk=r%9 or 9
+        self.loc_dict[x,y]=self
+    def __repr__(self):
+        return f"<{self.x,self.y}: risk={self.risk}; weight={self.weight}>"
+    def update_best_path(self):
+        x,y=self.x,self.y
+        adjs=(self.loc_dict[x+dx,y+dy] for dx,dy in DXDY if (x+dx,y+dy) in self.loc_dict)
+        self.weight=min(self.weight,min(adjs).weight+self.risk)
+
+with open('15.txt') as file:
+    data = file.read()
+grid = [list(map(int,row)) for row in data.splitlines()]
+
+
+PART2=True
+if PART2:
+    import numpy as np
+    ng=np.array(grid)
+    row=np.concatenate(tuple(ng+i for i in range(5)),axis=1)
+    grid=np.concatenate(tuple(row+i for i in range(5)),axis=0)
+
+TARGET=(len(grid[0])-1,len(grid)-1)
+
+q=[]
+for x in range(len(grid[0])):
+    for y in range(len(grid)):
+        q.append(BestWeightToPoint(x,y,grid[y][x]))
+q[0].weight=0  # Set 0,0 cost to 0
+for _ in range(10):
+    for p in q:
+        p.update_best_path()
+    print(p)
+
+print(f'Part {1+int(PART2)}:',BestWeightToPoint.loc_dict[TARGET].weight)
