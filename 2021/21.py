c5b7e25d82e1f61d9c547e4388898864122b76a3
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-21 19:18:35 +1300

    2021 Day 21 - Tidy up
    Wrap 'roll' into its own method, turn the while loop into an empty loop, and change the recursive function to swap whose turn it is by swapping the players around, shortening the code (and maybe memoizing a little more)

--- 2021/21.py
+++ 2021/21.py
@@ -18,33 +18,26 @@ class Player:
             self.position+=self.roll()
         self.position = self.position%10 or 10
         self.score+=self.position
-    def roll(self):
-        Player.die_count+=1
-        return next(Player.die)
+        return self.score<1000
+    @classmethod
+    def roll(cls):
+        cls.die_count+=1
+        return next(cls.die)
 
 player1 = Player(p1start)
 player2 = Player(p2start)
-while True:
-    player1.turn()
-    if player1.score>=1000: break
-    player2.turn()
-    if player2.score>=1000: break
+while player1.turn() and player2.turn(): pass
 print('Part 1:',min(player1.score,player2.score)*Player.die_count)
 
 FINAL_SCORE = 21
 THREE_ROLL_OUTCOMES = Counter(map(sum,product((1,2,3),repeat=3)))
 @cache
-def winners(p1,p2,s1=0,s2=0,t1=True):
-    if s1>=FINAL_SCORE: return (1,0)
+def winners(p1,p2,s1=0,s2=0):
     if s2>=FINAL_SCORE: return (0,1)
     w1=w2=0
     for roll,weight in THREE_ROLL_OUTCOMES.items():
-        if t1:
-            np1 = (p1+roll)%10 or 10
-            tw1,tw2 = winners(np1,p2,s1+np1,s2,not t1)
-        else:
-            np2 = (p2+roll)%10 or 10
-            tw1,tw2 = winners(p1,np2,s1,s2+np2,not t1)
+        np1 = (p1+roll)%10 or 10
+        tw2,tw1 = winners(p2,np1,s2,s1+np1)
         w1+=weight*tw1
         w2+=weight*tw2
     return w1,w2
