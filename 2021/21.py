138b440bcc3cd4709e596f7ac2fec590e80b968e
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-22 02:48:59 +1300

    2021 Day 21 - Replace magic numbers
    Replace magic numbers with constants, making it easier to try things like https://www.reddit.com/r/adventofcode/comments/rlbuxh/2021_day_21_part_3_what_if_the_die_was_a_d6/

--- 2021/21.py
+++ 2021/21.py
@@ -2,10 +2,13 @@ from itertools import cycle,product
 from collections import Counter
 from functools import cache
 
-## INPUT ##
-p1start = 7
-p2start = 4
-###########
+ROLLS = 3
+SIDES = 3
+TRACK_SIZE = 10
+
+####### INPUT #######
+p1start,p2start = 7,4
+#####################
 
 class Player:
     die = cycle(range(1,101))
@@ -14,9 +17,9 @@ class Player:
         self.position = start
         self.score = 0
     def turn(self):
-        for _ in range(3):
+        for _ in range(ROLLS):
             self.position+=self.roll()
-        self.position = self.position%10 or 10
+        self.position = self.position%TRACK_SIZE or TRACK_SIZE
         self.score+=self.position
         return self.score<1000
     @classmethod
@@ -30,13 +33,13 @@ while player1.turn() and player2.turn(): pass
 print('Part 1:',min(player1.score,player2.score)*Player.die_count)
 
 FINAL_SCORE = 21
-THREE_ROLL_OUTCOMES = Counter(map(sum,product((1,2,3),repeat=3)))
+ROLL_OUTCOMES = Counter(map(sum,product(range(1,SIDES+1),repeat=ROLLS)))
 @cache
 def winners(p1,p2,s1=0,s2=0):
     if s2>=FINAL_SCORE: return (0,1)
     w1=w2=0
-    for roll,weight in THREE_ROLL_OUTCOMES.items():
-        np1 = (p1+roll)%10 or 10
+    for roll,weight in ROLL_OUTCOMES.items():
+        np1 = (p1+roll)%TRACK_SIZE or TRACK_SIZE
         tw2,tw1 = winners(p2,np1,s2,s1+np1)
         w1+=weight*tw1
         w2+=weight*tw2
