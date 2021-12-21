77779434060dfbf4e7bf90d05ff37d2ac43b9552
Allan Taylor <allan.taylor.pi@gmail.com>
2021-12-22 02:58:59 +1300

    2021 Day 16 - Wrap solution
    Wrap the solution (both parts, optional visualisation) in a function so that I can test it with a range of inputs and snippets. Also tidy up process_tree to use a list of functions rather than if/else branches. Also use math.prod rather than reduce for the product. The version sum is now calculated using the tree structure, rather than a global counter.

--- 2021/16.py
+++ 2021/16.py
@@ -1,10 +1,5 @@
-from functools import reduce
-VERSION_SUM=0
-
 def parse_packet(packet,i=0):
     ver=int(packet[i:i+3],2)
-    global VERSION_SUM
-    VERSION_SUM+=ver
     pid=int(packet[i+3:i+6],2)
     if pid==4: # Literal value
         j=i+6
@@ -15,58 +10,49 @@ def parse_packet(packet,i=0):
                 break
             j+=5
         lit = int(rep,2)
-        return (ver,pid,lit),j+5
+        return (ver,pid,lit),j+5,ver
     len_type_id = packet[i+6]
+    sub_pacs=[]
+    v_sum=ver
     if len_type_id=='0':
         total_len_subpackets = int(packet[i+7:i+22],2)
         j=i+22
-        sub_pacs=[]
         while j<i+22+total_len_subpackets:
-            pac,j = parse_packet(packet,j)
+            pac,j,v = parse_packet(packet,j)
             sub_pacs.append(pac)
-        return (ver,pid,sub_pacs),j
+            v_sum+=v
     elif len_type_id=='1':
         num_sub_packets = int(packet[i+7:i+18],2)
         j=i+18
-        sub_pacs=[]
         for _ in range(num_sub_packets):
-            pac,j = parse_packet(packet,j)
+            pac,j,v = parse_packet(packet,j)
             sub_pacs.append(pac)
-        return (ver,pid,sub_pacs),j
-    else: raise ValueError(len_type_id)
+            v_sum+=v
+    return (ver,pid,sub_pacs),j,v_sum
+
+from math import prod
+def gt(v):return int(int.__gt__(*v))
+def lt(v):return int(int.__lt__(*v))
+def eq(v):return int(int.__eq__(*v))
 
 def process_tree(tree):
     ver,pid,val = tree
-    if isinstance(val,int):
-        return val
-    vals=map(process_tree,val)
-    if pid==0:
-        return sum(vals)
-    if pid==1:
-        return reduce(int.__mul__,vals)
-    if pid==2:
-        return min(vals)
-    if pid==3:
-        return max(vals)
-    a,b = vals
-    if pid==5:
-        return int(a>b)
-    if pid==6:
-        return int(a<b)
-    if pid==7:
-        return int(a==b)
+    if pid==4:return val
+    return (sum,prod,min,max,None,gt,lt,eq)[pid](map(process_tree,val))
 
-def tree_view(tree,solve=False,depth=0,last=True,depths=set(),OPS='+*v^ ><='):
+def hex2bin(hex_str):return format(int(hex_str,16),f'0{len(hex_str)*4}b')
+
+def tree_view(tree,solve=False,depth=0,last=True,depths=set(),OPS='+*v^#><='):
     """Prints the tree (and optionally, the solved value at each node)
     tree: tuple(version, type_id, [subtrees, ...]|int)
     solve: bool - prints the calculated value of each node"""
     depths.add(depth)
     if last: depths.discard(depth)
     ver,pid,val = tree
-    out="".join((' │'[d in depths] for d in range(depth)))\
-        +('├└'[last])+f"V:{ver};T:{pid} ({OPS[pid]})"
+    out="".join((' │'[d in depths] for d in range(1,depth)))\
+        +('├└'[last] if depth else '')+f"V:{ver};T:{pid} ({OPS[pid]})"
     if isinstance(val,int):
-        print(out[:-4],f"[{val}]")
+        print(out,f"[{val}]")
     else:
         if solve:
             out+=f" = {process_tree(tree)}"
@@ -75,12 +61,17 @@ def tree_view(tree,solve=False,depth=0,last=True,depths=set(),OPS='+*v^ ><='):
             tree_view(v,solve,depth+1,False)
         tree_view(val[-1],solve,depth+1)
 
+def evaluate(hex_input,view=True,solve=True):
+    binput = hex2bin(hex_input.strip())
+    tree,j,ver_sum=parse_packet(binput)
+    solution=process_tree(tree)
+    if view:
+        print('Version Sum:',ver_sum)
+        print('Trailing 0s:',binput[j:])
+        print('Solution:',solution)
+        tree_view(tree,solve)
+    return ver_sum,solution
+
 with open('16.txt') as file:
-    data = file.read().strip()
-
-packet=format(int(data,16),f'0{len(data)*4}b')
-tree,_ = parse_packet(packet)
-
-print('Part 1:',VERSION_SUM)
-print('Part 2:',process_tree(tree))
-tree_view(tree,True)
+    data = file.read()
+print('Part 1: {}\nPart 2: {}'.format(*evaluate(data)))
