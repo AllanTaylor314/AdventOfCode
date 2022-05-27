d3c5c66fde28fd21182e8aee4a221940067d0f46
Allan Taylor <allan.taylor.pi@gmail.com>
2022-05-28 00:18:51 +1200

    2019 Day 12 Part 2
    Finally got around to doing this. Had to get a hint from the megathread: x, y, and z are independent, so the lcm of each direction's period is the solution (my initial brute force was never going to work)

--- 2019/12.py
+++ 2019/12.py
@@ -8,13 +8,13 @@ USE_TURTLE=False
 if USE_TURTLE:
     from turtle import *
 
-def sign(a):return int(a/abs(a)) if a else 0
+def sign(a):return 1 if a>0 else -1 if a<0 else 0
 
 class Moon:
     def __init__(self, x, y, z):
         self.x,self.y,self.z=x,y,z
         self.vx,self.vy,self.vz=0,0,0
-        self.xs,self.ys,self.zs=[x],[y],[z]
+        if PLOT: self.xs,self.ys,self.zs=[x],[y],[z]
         if USE_TURTLE:
             self.turtle=Turtle()
             self.turtle.hideturtle()
@@ -30,14 +30,27 @@ class Moon:
         self.x+=self.vx
         self.y+=self.vy
         self.z+=self.vz
-        self.xs.append(self.x)
-        self.ys.append(self.y)
-        self.zs.append(self.z)
+        if PLOT:
+            self.xs.append(self.x)
+            self.ys.append(self.y)
+            self.zs.append(self.z)
         if USE_TURTLE:self.turtle.goto(self.x,self.y)
     def energy(s):
         return sum(map(abs,(s.x,s.y,s.z)))*sum(map(abs,(s.vx,s.vy,s.vz)))
     def __repr__(s):
         return f"p=<{s.x:4d},{s.y:4d},{s.z:4d}>; v=<{s.vx:4d},{s.vy:4d},{s.vz:4d}>"
+    def get_state(self, axes):
+        """Part 2 - x,y,z are independent
+        return position, velocity"""
+        return getattr(self,axes),getattr(self,f"v{axes}")
+
+def get_all_states():
+    global moons
+    states = {'x':(),'y':(),'z':()}
+    for moon in moons:
+        for a in 'xyz':
+            states[a]+=moon.get_state(a)
+    return states.values() # Note - sorted dict
 
 with open('12.txt') as file:
     data = file.read().splitlines()
@@ -51,13 +64,43 @@ for d in data:
     moons.append(Moon(x,y,z))
 Io, Europa, Ganymede, Callisto = moons
 
-for step in range(1000):
+state_x = {}
+state_y = {}
+state_z = {}
+xdiff = ydiff = zdiff = None
+
+step = 0
+while xdiff is None or ydiff is None or zdiff is None or step<1000:
     for moon in moons:
         for other_moon in moons:
             moon.apply_gravity(other_moon)
     for moon in moons:
         moon.apply_velocity()
-print('Part 1:',sum(moon.energy() for moon in moons))
+    if step==999:
+        print('Part 1:',sum(moon.energy() for moon in moons),flush=True)
+    x,y,z = get_all_states()
+    if xdiff is None:
+        if x in state_x:
+            xdiff = step-state_x[x]
+            print('x',xdiff,flush=True)
+        else:
+            state_x[x]=step
+    if ydiff is None:
+        if y in state_y:
+            ydiff = step-state_y[y]
+            print('y',ydiff,flush=True)
+        else:
+            state_y[y]=step
+    if zdiff is None:
+        if z in state_z:
+            zdiff = step-state_z[z]
+            print('z',zdiff,flush=True)
+        else:
+            state_z[z]=step
+    step+=1
+
+from math import lcm
+print('Part 2:',lcm(xdiff,ydiff,zdiff))
 
 if PLOT:
     fig = plt.figure()
