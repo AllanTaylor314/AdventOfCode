7e5451f5c638ebdd13a4449939c06487b4ef92db
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-05 18:11:38 +1300

    2022 Day 5

---
+++ 2022/05.py
@@ -0,0 +1,23 @@
+with open("05.txt") as file:
+    raw_stacks,raw_lines = file.read().split("\n\n")
+    lines = raw_lines.splitlines()
+instructions = []
+stacks = [[] for _ in range(10)] # 0 is empty filler
+for row in raw_stacks.splitlines()[-2::-1]:
+    for i,c in enumerate(row[1::4],start=1):
+        if c!=" ": stacks[i].append(c)
+for line in lines:
+    _,n,_,s,_,d = line.split()
+    instructions.append(tuple(map(int,(n,s,d))))
+stacks2 = [s.copy() for s in stacks]
+
+for n,s,d in instructions:
+    for _ in range(n):
+        stacks[d].append(stacks[s].pop())
+    
+    stacks2[d].extend(stacks2[s][-n:])
+    del stacks2[s][-n:]
+p1 = [s[-1] for s in stacks[1:]]
+print("Part 1: ",*p1,sep="")
+p2 = [s[-1] for s in stacks2[1:]]
+print("Part 2: ",*p2,sep="")
