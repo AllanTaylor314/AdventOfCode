94154fe7af33bad2ea8ae215ff082cfca342cd72
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-06 18:11:09 +1300

    2022 Day 6 - tidy up and generalise as a function

--- 2022/06.py
+++ 2022/06.py
@@ -1,13 +1,10 @@
+def unique_offset(data,length):
+    for i in range(length,len(data)):
+        if len(set(data[i-length:i]))==length:
+            return i
+
 with open("06.txt") as file:
     data = file.read().strip()
 
-for i,four in enumerate(zip(data,data[1:],data[2:],data[3:]),start=4):
-    if len(set(four))==4:
-        p1 = i
-        break
-print("Part 1:",p1)
-for i in range(14,len(data)):
-    if len(set(data[i-14:i]))==14:
-        p2=i
-        break
-print("Part 2:",p2)
+print("Part 1:",unique_offset(data,4))
+print("Part 2:",unique_offset(data,14))
