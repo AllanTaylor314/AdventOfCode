85713c47e6e08219c3f5084ffb83fda4e299226b
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-09 18:22:40 +1300

    2022 Day 9 - General solution

--- 2022/09.py
+++ 2022/09.py
@@ -1,36 +1,23 @@
 with open("09.txt") as file:
     lines = file.read().splitlines()
 deltas = [(int(b),1j**"RDLU".index(a)) for a,b in map(str.split,lines)] # num, dir
-directions = [1,1+1j,1j,-1+1j,-1,-1-1j,-1j,1-1j]
 def sign(n):
     if n==0:return 0
     if n<0:return -1
     return 1
 def difference_to_direction(diff: complex):
     return sign(diff.real)+sign(diff.imag)*1j
-p1 = 0
-HEAD = 0+0j
-TAIL = 0+0j
-tails = {TAIL}
-for num,dire in deltas:
-    for _ in range(num):
-        HEAD+=dire
-        while abs(TAIL-HEAD)>=2: # Up to sqrt2
-            rough = HEAD-TAIL
-            TAIL+=difference_to_direction(rough)
-            tails.add(TAIL)
-    
-
-print("Part 1:",len(tails))
-p2 = 0
-tails2 = {0}
-rope = [0+0j]*10
-for num,dire in deltas:
-    for _ in range(num):
-        rope[0]+=dire
-        for ti in range(1,len(rope)):
-            hi = ti-1 # head index, tail index
-            while abs(rope[ti]-rope[hi])>=2: # Up to sqrt2
-                rope[ti]+=difference_to_direction(rope[hi]-rope[ti])
-        tails2.add(rope[ti]) # last only
-print("Part 2:",len(tails2))
+def solve(length):
+    tails = {0}
+    rope = [0+0j]*length
+    for num,dire in deltas:
+        for _ in range(num):
+            rope[0]+=dire
+            for ti in range(1,len(rope)):
+                hi=ti-1 # head index, tail index
+                while abs(rope[hi]-rope[ti])>=2: # Up to sqrt2
+                    rope[ti]+=difference_to_direction(rope[hi]-rope[ti])
+            tails.add(rope[ti]) # last only
+    return len(tails)
+print("Part 1:",solve(2))
+print("Part 2:",solve(10))
