387e7b922863a2a94fb2dc2c4c25aad52ee611c3
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-12 18:28:53 +1300

    2022 Day 12 Part 1

---
+++ 2022/12.py
@@ -0,0 +1,38 @@
+from collections import deque
+def valid_steps(z):
+    global grid
+    c = grid[z]
+    for i in range(4):
+        nz = z+1j**i
+        if nz not in grid:
+            continue
+        nc = grid[nz]
+        if ord(nc)<=ord(c)+1:
+            yield nz
+
+with open("12.txt") as file:
+    lines = file.read().splitlines()
+# lines = """Sabqponm
+# abcryxxl
+# accszExk
+# acctuvwj
+# abdefghi""".splitlines()
+grid = {i+j*1j:c for i,row in enumerate(lines) for j,c in enumerate(row)}
+start ,= (z for z,c in grid.items() if c=='S')
+end ,= (z for z,c in grid.items() if c=='E')
+grid[start]='a'
+grid[end]='z'
+
+q = deque([(start,0)])
+dists = {start:0}
+while q:
+    z,d = q.popleft()
+    for w in valid_steps(z):
+        if w not in dists:
+            dists[w]=d+1
+            q.append((w,d+1))
+    # print(q)
+print("Part 1:",dists[end]) # not 445
+p2 = 0
+
+print("Part 2:",p2)
