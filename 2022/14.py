ad43ae6af7388c7e389f045540e82328238e88f4
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-14 19:31:57 +1300

    2022 Day 14 Part 2 - sloooow (because I did this via remote desktop from a phone. I'll do a BFS later)

--- 2022/14.py
+++ 2022/14.py
@@ -7,14 +7,24 @@ with open("14.txt") as file:
 pair_lists = [[tuple(map(int,pair.split(","))) for pair in line.split(" -> ")] for line in lines]
 imag_lists = [[x+y*1j for x,y in l] for l in pair_lists]
 WALLS = {n for il in imag_lists for a,b in zip(il,il[1:]) for n in imag_range(a,b)}
-BOTTOM_Y = int(max(z.imag for z in WALLS))
+BOTTOM_Y = int(max(z.imag for z in WALLS))+1
 SANDS = set()
 sand = 500+0j
-while sand.imag < BOTTOM_Y:
+while 500+0j not in SANDS:
     sand = 500+0j
     while True:
         full = WALLS | SANDS
-        if sand+1j not in full:
+        if sand in full:
+            print(len(SANDS))
+            break
+        if sand.imag == BOTTOM_Y:
+            SANDS.add(sand)
+            print(len(SANDS))
+            break
+        elif sand.imag > BOTTOM_Y:
+            print(len(SANDS))
+            break
+        elif sand+1j not in full:
             sand+=1j
         elif sand-1+1j not in full:
             sand+=-1+1j
@@ -22,11 +32,13 @@ while sand.imag < BOTTOM_Y:
             sand+=1+1j
         else: # at rest
             SANDS.add(sand)
+            print(len(SANDS))
             break
-        if sand.imag >= BOTTOM_Y:
-            break
-    if sand.imag >= BOTTOM_Y:
-        break
+      #  if sand.imag == BOTTOM_Y:
+       #     SANDS.add(sand)
+          #  break
+  #  if sand.imag >= BOTTOM_Y:
+    #    break
 
 p1 = len(SANDS)
 print("Part 1:",p1)
