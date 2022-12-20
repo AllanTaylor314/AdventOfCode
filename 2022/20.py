4304274e891f7f29bb24862725436f18e034d408
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-20 18:44:44 +1300

    2022 Day 20 Part 1&2 (Boolean toggle)

--- 2022/20.py
+++ 2022/20.py
@@ -31,44 +31,27 @@ class CLLNode:
         assert self.next.prev is self.prev.next is self
         assert self.next is not self is not self.prev
 
-def print_cll(node):
-    print(end=f"[{node!r}")
-    curr = node.next
-    while curr is not node:
-        print(end=f", {curr!r}")
-        curr = curr.next
-    print("]")
+PART2 = False
 
 with open("20.txt") as file:
     lines = file.read().splitlines()
 nums = list(map(int,lines))
-# nums = [1,2,-3,3,-2,0,4]
-nums = [n*811589153 for n in nums]
+if PART2:nums = [n*811589153 for n in nums]
 nodes = list(map(CLLNode,nums))
-for a,b in zip(nodes,nodes[1:]):
+
+for a,b in zip(nodes,nodes[1:]+nodes[:1]):
     a.next=b
     b.prev=a
-nodes[0].prev=nodes[-1]
-nodes[-1].next=nodes[0]
-for _ in range(10):
+
+for _ in range(10 if PART2 else 1):
     for node in nodes:
-        # print(end=f"Moving {node}...")
         node.move()
-        # print("Done")
-for node in nodes:
-    assert node.next.prev is node.prev.next is node, f"{node} is not quite right"
-p1 = 0
-curr = nodes[0]
-c=0
-while curr.item!=0:
-    c+=1
-    curr=curr.next
-    if c>10000:
-        exit()
-print(curr)
+
+curr = nodes[nums.index(0)]
+answer = 0
 for i in range(3):
     for _ in range(1000):
         curr=curr.next
     print(curr)
-    p1+=curr.item
-print("Part 2:",p1)
+    answer+=curr.item
+print(f"Part {1+PART2}:",answer)
