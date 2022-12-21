262468c767931307f376261c932c691d096af048
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-21 19:00:13 +1300

    2022 Day 21 - Clean up output format

--- 2022/21.py
+++ 2022/21.py
@@ -12,9 +12,8 @@ def solve(name):
     if isinstance(monkeys[name],(int,sp.Symbol,sp.Integer)):
         return monkeys[name]
     name1, op, name2 = monkeys[name].split()
-    # if op=='/':op='//'
-    return (parse_expr(f"(({solve(name1)}) {op} ({solve(name2)}))"))
+    return (parse_expr(f"({solve(name1)}) {op} ({solve(name2)})")) # THESE BRACKETS!!!!
 print("Part 1:",int(solve("root")))
 left,_,right = monkeys["root"].split()
 monkeys["humn"]=humn=sp.Symbol("humn")
-print(sp.solve(solve(left)-solve(right),humn))
+print("Part 2:",*sp.solve(solve(left)-solve(right),humn))
