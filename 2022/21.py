a6bf35a2ae00571667750257d0398ea05fb0693b
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-21 18:48:19 +1300

    2022 Day 21 Part 2

--- 2022/21.py
+++ 2022/21.py
@@ -1,15 +1,20 @@
+import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
 with open("21.txt") as file:
     lines = file.read().splitlines()
 monkeys = {}
 for line in lines:
     name,ops = line.split(": ")
-    if ops.isdigit():
-        ops=int(ops)
+    try:ops=int(ops)
+    except ValueError:pass
     monkeys[name]=ops
 def solve(name):
-    if isinstance(monkeys[name],int):
+    if isinstance(monkeys[name],(int,sp.Symbol,sp.Integer)):
         return monkeys[name]
     name1, op, name2 = monkeys[name].split()
-    return int(eval(f"{solve(name1)} {op} {solve(name2)}"))
-print("Part 1:",solve("root"))
-
+    # if op=='/':op='//'
+    return (parse_expr(f"(({solve(name1)}) {op} ({solve(name2)}))"))
+print("Part 1:",int(solve("root")))
+left,_,right = monkeys["root"].split()
+monkeys["humn"]=humn=sp.Symbol("humn")
+print(sp.solve(solve(left)-solve(right),humn))
