e4f6cf32c14a60fdb07e9b19568adf3f19f1a3d1
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-22 20:20:09 +1300

    2022 Day 22 - remove example input

--- 2022/22.py
+++ 2022/22.py
@@ -2,22 +2,6 @@ import re
 L=-1j;R=1j
 with open("22.txt") as file:
     raw_map,raw_instructions = file.read().split("\n\n")
-if 0:
-    raw_map,raw_instructions="""        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5
-""".split("\n\n")
 instructions = re.findall(r"(\d+|R|L)",raw_instructions)
 empties = set()
 walls = set()
@@ -35,25 +19,19 @@ for x in range(1,num_cols+1):
     for y in range(-1,num_rows+2):
         if complex(x,y) not in on_map and complex(x,y+1) in on_map:
             start = complex(x,y)
-            # print(f"Start for {x=}: {start}")
         if complex(x,y) not in on_map and complex(x,y-1) in on_map:
             portals[start,-1j]=complex(x,y-1)
             portals[complex(x,y),1j]=start+1j
             break
-    # else:
-    #     print(f"No portal on for {x=}")
 
 for y in range(1,num_rows+1):
     for x in range(-1,num_cols+2):
         if complex(x,y) not in on_map and complex(x+1,y) in on_map:
             start = complex(x,y)
-            # print(f"Start for {y=}: {start}")
         if complex(x,y) not in on_map and complex(x-1,y) in on_map:
             portals[start,-1]=complex(x-1,y)
             portals[complex(x,y),1]=start+1
             break
-    # else:
-    #     print(f"No portal on for {y=}")
 d=d0=1 # Right
 p=p0=min(empties,key=lambda z:(z.imag,z.real))
 
@@ -118,4 +96,4 @@ for ins in instructions:
             assert np in empties
             p=np
             d=nd
-print("Part 2:",int(1000*p.imag+4*p.real+(1,1j,-1,-1j).index(d))) # 14520, 19415 too low
+print("Part 2:",int(1000*p.imag+4*p.real+(1,1j,-1,-1j).index(d)))
