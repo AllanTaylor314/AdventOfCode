ff09204c8da56f91ab88fc9e8f543ffad0b98cad
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-24 12:02:52 +1300

    2022 Day 23 - Name direction constants

--- 2022/23.py
+++ 2022/23.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
+N=-1j;S=1j;W=-1;E=1
 
 with open("23.txt") as file:
     lines = file.read().splitlines()
 init_positions = set(complex(x,y) for y,row in enumerate(lines) for x,c in enumerate(row) if c=="#")
 positions = set(init_positions)
 proposals = [
-    lambda elf:(elf-1j,elf-1-1j,elf+1-1j), # N
-    lambda elf:(elf+1j,elf-1+1j,elf+1+1j), # S
-    lambda elf:(elf-1,elf-1-1j,elf-1+1j),  # W
-    lambda elf:(elf+1,elf+1-1j,elf+1+1j)   # E
+    lambda elf:(elf+N,elf+N+E,elf+N+W), # N
+    lambda elf:(elf+S,elf+S+E,elf+S+W), # S
+    lambda elf:(elf+W,elf+N+W,elf+S+W), # W
+    lambda elf:(elf+E,elf+N+E,elf+S+E)  # E
 ]
 r=0
 num_moved = -1
@@ -23,7 +24,7 @@ while num_moved:
     num_moved=0
     new_positions = defaultdict(list)
     for elf in positions:
-        if not {elf+1,elf+1+1j,elf+1j,elf-1+1j,elf-1,elf-1-1j,elf-1j,elf+1-1j}&positions: # All empty
+        if not {elf+E,elf+S+E,elf+S,elf+S+W,elf+W,elf+N+W,elf+N,elf+N+E}&positions: # All empty
             new_positions[elf].append(elf)
             continue
         for prop in proposals:
