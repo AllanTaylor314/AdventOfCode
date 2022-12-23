8f8c3a3a8236812175f2a1cf90e95810d4c492ad
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-23 18:44:03 +1300

    2022 Day 23 Part 2

--- 2022/23.py
+++ 2022/23.py
@@ -20,7 +20,17 @@ proposals = [
     lambda elf:(elf-1,elf-1-1j,elf-1+1j),  # W
     lambda elf:(elf+1,elf+1-1j,elf+1+1j)   # E
 ]
-for r in range(10): # 10 rounds
+r=0
+num_moved = -1
+while num_moved:
+    if r==10:
+        minx=min(int(z.real) for z in positions)
+        maxx=max(int(z.real) for z in positions)
+        miny=min(int(z.imag) for z in positions)
+        maxy=max(int(z.imag) for z in positions)
+        p1 = (maxx-minx+1)*(maxy-miny+1)-len(positions)
+        print("Part 1:",p1) # Not 5994, 6474, 4018
+    num_moved=0
     new_positions = defaultdict(list)
     for elf in positions:
         if not {elf+1,elf+1+1j,elf+1j,elf-1+1j,elf-1,elf-1-1j,elf-1j,elf+1-1j}&positions: # All empty
@@ -32,18 +42,13 @@ for r in range(10): # 10 rounds
                 break
         else:
             new_positions[elf].append(elf)
+    old_positions = positions
     positions={(pro if len(elves)==1 else elf) for pro,elves in new_positions.items() for elf in elves}
+    num_moved = len(old_positions-positions)
     assert len(positions)==len(init_positions) # Did we lose any elves?
-    print(r+1)
-    print_map(positions)
+    r+=1
+    print(f"End of round {r}: Moved {num_moved} elves")
+    # print_map(positions)
     proposals.append(proposals.pop(0))
 
-minx=min(int(z.real) for z in positions)
-maxx=max(int(z.real) for z in positions)
-miny=min(int(z.imag) for z in positions)
-maxy=max(int(z.imag) for z in positions)
-p1 = (maxx-minx+1)*(maxy-miny+1)-len(positions)
-print("Part 1:",p1) # Not 5994, 6474, 4018
-p2 = 0
-
-print("Part 2:",p2)
+print("Part 2:",r)
