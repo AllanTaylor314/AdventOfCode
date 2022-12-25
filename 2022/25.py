3bd8f9975cd6758bf07f4227cb6d4fab0583fbd7
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-26 00:18:34 +1300

    1=11-2 Day 100 - Make it work for negative numbers too

--- 2022/25.py
+++ 2022/25.py
@@ -3,11 +3,11 @@ def snafu_to_decimal(snafu):
     if snafu:
         return 5*snafu_to_decimal(snafu[:-1])+PLACES[snafu[-1]]
     return 0
-def decimal_to_snafu(decimal):
-    if decimal>0:
+def decimal_to_snafu(decimal,_root_call=True):
+    if decimal:
         a,b=divmod(decimal+2,5)
-        return decimal_to_snafu(a)+"=-012"[b]
-    return ""
+        return decimal_to_snafu(a,False)+"=-012"[b]
+    return "0" if _root_call else ""
 with open("25.txt") as file:
     lines = file.read().splitlines()
 p1 = sum(map(snafu_to_decimal,lines))
