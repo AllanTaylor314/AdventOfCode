b2d01570002a991348aadb72b96d00f0cf44bc94
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-25 18:22:43 +1300

    2022 Day 25 - tidy up

--- 2022/25.py
+++ 2022/25.py
@@ -8,25 +8,7 @@ def decimal_to_snafu(decimal):
         a,b=divmod(decimal+2,5)
         return decimal_to_snafu(a)+"=-012"[b]
     return ""
-# print(*map(snafu_to_decimal,"""1=-0-2
-# 12111
-# 2=0=
-# 21
-# 2=01
-# 111
-# 20012
-# 112
-# 1=-1=
-# 1-12
-# 12
-# 1=
-# 122""".splitlines()),sep="\n")
 with open("25.txt") as file:
     lines = file.read().splitlines()
-
 p1 = sum(map(snafu_to_decimal,lines))
-
 print("Part 1:",decimal_to_snafu(p1))
-p2 = 0
-
-print("Part 2:",p2)
