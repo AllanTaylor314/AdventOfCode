6caff5793b851266dbc16ca6f066a7abd0c66d0b
Allan Taylor <allan.taylor.pi@gmail.com>
2022-12-28 23:12:25 +1300

    Revamp fetch_input.py so that it can be used to get the input for any day and year (and also put inputs in the new submodule)

--- fetch_input.py
+++ fetch_input.py
@@ -14,48 +14,81 @@ import sys
 import os
 from pathlib import Path
 import requests
+from time import sleep
 from dotenv import load_dotenv
 load_dotenv()
 LOGIN_ERROR = b"Puzzle inputs differ by user.  Please log in to get your puzzle input.\n"
 EARLY_ERROR = b"Please don't repeatedly request this endpoint before it unlocks! The calendar countdown is synchronized with the server time; the link will be enabled on the calendar the instant this puzzle becomes available.\n"
 SESSION = os.environ["SESSION"]
 BASE_PATH = Path(os.path.dirname(os.path.realpath(__file__))) # Let this be run from anywhere and act in the right place
+TEMPLATE_INPUT = "inputs/{year}/{day:02d}.txt" # Relative to this file
+TEMPLATE_URL = "https://adventofcode.com/{year}/day/{day}/input"
+PREVIEW_COLS = 80
+PREVIEW_ROWS = 5
+
+def fetch_input(day,year,preview=False):
+    dir_year = BASE_PATH / f"inputs/{year}"
+    if not os.path.isdir(dir_year):
+        print(f"No directory for {year}...")
+        os.mkdir(dir_year)
+        print("Created directory")
+    file_input = BASE_PATH / TEMPLATE_INPUT.format(day=day,year=year)
+    url = TEMPLATE_URL.format(day=day,year=year)
+    if os.path.isfile(file_input):
+        print(f"Looks like you've got the {year} Day {day} input already")
+    else:
+        print(end=f"Fetching the {year} Day {day} input...")
+        r = requests.get(url, cookies={"session":SESSION}, headers={"User-Agent":"https://github.com/AllanTaylor314/AdventOfCode/blob/main/fetch_input.py by allan.taylor.pi@gmail.com"})
+        print("Got it!")
+        if r.content == LOGIN_ERROR:
+            print("Whoops - you're not logged in!")
+            sys.exit(1)
+        if r.content == EARLY_ERROR:
+            print("Whoops - too soon!")
+            return False
+        text = r.content.decode("utf8")
+        if preview:
+            lines = text.splitlines()
+            trimmed_lines = [line[:PREVIEW_COLS]+(f"... [+{len(line)-PREVIEW_COLS}]" if len(line)>PREVIEW_COLS else "") for line in lines[:PREVIEW_ROWS]]
+            if len(lines)>PREVIEW_ROWS:trimmed_lines.append(f"... [{len(lines)-PREVIEW_ROWS} more lines]")
+            print("\n".join(trimmed_lines))
+        with open(file_input, "wb") as file:
+            file.write(r.content)
+        print(f"Saved to {file_input}")
+        return True
+
 
 now = datetime.now(tz=pytz.timezone('US/Eastern')) + timedelta(minutes=1) # because my computer is slightly out of sync
 # now = datetime(2015,12,5)
-print(end=f"Advent of Code {now.year}")
-
-if now.month != 12:
-    print("\nIt's not December yet!")
-    sys.exit(1)
-elif now.day > 25:
-    print("\nBit late for an advent calendar now")
-    sys.exit(1)
-else:
-    print(f", Day {now.day}")
-
-dir_year = BASE_PATH / f"./{now.year}"
-if not os.path.isdir(dir_year):
-    print("No directory for current year...")
-    os.mkdir(dir_year)
-    print("Created directory")
-
-file_input = BASE_PATH / f"{now.year}/{now.day:02d}.txt"
-url = f"https://adventofcode.com/{now.year}/day/{now.day}/input"
-if os.path.isfile(file_input):
-    print("Looks like you've got today's input already")
-else:
-    print(end="Fetching the input...")
-    r = requests.get(url, cookies={"session":SESSION}, headers={"User-Agent":"https://github.com/AllanTaylor314/AdventOfCode/blob/main/fetch_input.py by allan.taylor.pi@gmail.com"})
-    print("Got it!")
-    if r.content == LOGIN_ERROR:
-        print("Whoops - you're not logged in!")
+print(end="Advent of Code")
+year = now.year
+day = None
+if len(sys.argv)==1:
+    if now.month != 12:
+        print("\nIt's not December yet!")
         sys.exit(1)
-    if r.content == EARLY_ERROR:
-        print("Whoops - too soon!")
+    elif now.day > 25:
+        print("\nBit late for an advent calendar now")
         sys.exit(1)
-    text = r.content.decode("utf8")
-    print(text[:100]+("..." if len(text)>100 else ""))
-    with open(file_input, "wb") as file:
-        file.write(r.content)
-    print(f"Saved to {file_input}")
+    else:
+        print(f" {year}, Day {day}")
+        day = now.day
+        fetch_input(day,year,preview=True)
+else:print()
+for val in sys.argv[1:]:
+    try:
+        num = int(val)
+    except ValueError:
+        print(f"{val!r} could not be interpreted as an integer")
+        continue
+    if 0<num<=25:
+        day=num
+        if fetch_input(day,year):
+            print(end="Waiting 5 seconds for ratelimiting...",flush=True)
+            sleep(5)
+            print("OK")
+    elif 2015<=num<=now.year:
+        year=num
+        print(f"Set year to {year}")
+    else:
+        print(f"{num} could not be interpreted as a day or year")
