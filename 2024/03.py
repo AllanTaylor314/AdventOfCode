d218e27cddc092107e5931ce65c63995b3155498
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-03 18:31:13 +1300

    2024 Day 3 - swap "do()" and "don't()" splits for simpler code & factor out regex pattern

--- 2024/03.py
+++ 2024/03.py
@@ -9,16 +9,16 @@ timer_parse_start=perf_counter()
 ############################## PARSER ##############################
 with open(INPUT_PATH) as file:
     lines = file.read()
-
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
 timer_parse_end=timer_part1_start=perf_counter()
 ############################## PART 1 ##############################
-pairs = (map(int,line) for line in re.findall(r"mul\((\d+),(\d+)\)",lines))
+pairs = (map(int,line) for line in re.findall(pattern,lines))
 p1 = sum(a*b for a,b in pairs)
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-data = [l for line in ("do()"+lines).split("don't()") for l in line.split("do()")[1:]]
-p2 = sum(a*b for a,b in [tuple(map(int,line)) for line in re.findall(r"mul\((\d+),(\d+)\)","X".join(data))])
+data = [line.split("don't()")[0] for line in lines.split("do()")]
+p2 = sum(a*b for a,b in (map(int,line) for line in re.findall(pattern,"X".join(data))))
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
 print(f"""Execution times (sec)
