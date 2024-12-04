558afeab465351c326856d747d6c15841cd698d7
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-05 01:25:33 +1300

    2024 Day 4 - Rectangle golfing

--- 2024/04g.py
+++ 2024/04g.py
@@ -1,5 +1,5 @@
-*l,g=*open(0),lambda i,j:l[i][j]if len(l)>i>-1<j<len(l[i])else""
-e,*d=enumerate,-1,0,1
-print(*map(sum,zip(*((all(c==g(i+n*x,j+n*y)for n,c in e("XMAS"))
-,x!=0!=y!=c=="A"<"M"==g(i-x,j-y)==g(i-y,j+x)<"S"==g(i+x,j+y)==g(
-i+y,j-x))for i,m in e(l)for j,c in e(m)for x in d for y in d))))
+*l,g=*open(0),lambda i,j:l[i][j]if len(l)>i>-1<j<len(l[i
+])else"";e,*d=enumerate,-1,0,1;print(*map(sum,zip(*((all
+(c==g(i+n*x,j+n*y)for n,c in e("XMAS")),x!=0!=y!=c=="A"<
+"M"==g(i-x,j-y)==g(i-y,j+x)<"S"==g(i+x,j+y)==g(i+y,j-x))
+for i,m in e(l)for j,c in e(m)for x in d for y in d))))
