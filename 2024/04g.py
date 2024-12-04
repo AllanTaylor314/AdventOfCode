fcddedd4dad28deba39ee089409c2fedacb33f3a
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-05 01:04:20 +1300

    2024 Day 4 - More golfing

--- 2024/04g.py
+++ 2024/04g.py
@@ -1,5 +1,5 @@
-g=lambda i,j:l[i][j]if len(l)>i>-1<j<len(l[i])else""
-*l,e,d=*open(0),enumerate,[(i,j)for i in(-1,0,1)for j in(-1,0,1)]
-print(sum(all(c==g(i+n*x,j+n*y)for n,c in e("XMAS"))for i,m in e(l)for j,c in
-e(m)for x,y in d),sum(c=="A"and"M"==g(i-x,j-y)==g(i-y,j+x)and"S"==g(i+x,j+y)
-==g(i+y,j-x)for i,m in e(l)for j,c in e(m)for x,y in d[::2]))
+*l,g=*open(0),lambda i,j:l[i][j]if len(l)>i>-1<j<len(l[i])else""
+e=enumerate;d=[(i,j)for i in(-1,0,1)for j in(-1,0,1)]
+print(*map(sum,zip(*((all(c==g(i+n*x,j+n*y)for n,c in e("XMAS"))
+,c=="A"and"M"==g(i-x,j-y)==g(i-y,j+x)and"S"==g(i+x,j+y)==g(i+y,j
+-x)and x!=0!=y)for i,m in e(l)for j,c in e(m)for x,y in d))))
