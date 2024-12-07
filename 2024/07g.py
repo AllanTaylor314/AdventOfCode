4a6b16d1233102bd73fa0f886bd6e91098d1c0b2
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-07 23:05:13 +1300

    2024 Day 7 - Even more golfing

--- 2024/07g.py
+++ 2024/07g.py
@@ -1,4 +1,4 @@
 from operator import*;O=add,mul
-C=[(int(a),[*map(int,b.split())])for a,b in(l.split(":")for l in open(False))]
+C=[(int(a[:-True]),[*map(int,b)])for a,*b in map(str.split,open(False))]
 g=lambda l,i=-True:{o(v,l[i])for v in g(l,~-i%len(l))for o in O}if i else{l[i]}
-for p in O:print(sum(c*(c in g(n))for c,n in C));O+=lambda a,b:int(f"{a}{b}"),
+for p in O:print(sum(c*(c in g(n))for c,n in C));O+=lambda*a:int("%d%d"%a),
