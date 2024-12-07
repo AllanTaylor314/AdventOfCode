a3358d072006cf5be398951afd9481adcaefe453
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-07 19:11:23 +1300

    2024 Day 7 - Replace dict with list of tuples
    The problem doesn't necessarily guarantee unique test numbers

--- 2024/07.py
+++ 2024/07.py
@@ -9,7 +9,7 @@ timer_parse_start=perf_counter()
 ############################## PARSER ##############################
 with open(INPUT_PATH) as file:
     lines = file.read().splitlines()
-cals = {int(a):list(map(int,b.split())) for a,b in (line.split(": ") for line in lines)}
+cals = [(int(a),list(map(int,b.split()))) for a,b in (line.split(": ") for line in lines)]
 
 timer_parse_end=timer_part1_start=perf_counter()
 ############################## PART 1 ##############################
@@ -23,12 +23,12 @@ def gen_vals(line,index=-1):
             for op in OPERATORS:
                 yield op(val, line[index])
 
-p1 = sum(cal for cal, nums in cals.items() if any(a==cal for a in gen_vals(nums)))
+p1 = sum(cal for cal, nums in cals if any(a==cal for a in gen_vals(nums)))
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
 OPERATORS.append(lambda a,b:int(f"{a}{b}"))
-p2 = sum(cal for cal, nums in cals.items() if any(a==cal for a in gen_vals(nums)))
+p2 = sum(cal for cal, nums in cals if any(a==cal for a in gen_vals(nums)))
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
 print(f"""Execution times (sec)
