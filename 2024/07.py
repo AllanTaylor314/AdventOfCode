957de3047789a01c1badb4b246b734f95e73d8f5
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-08 02:51:35 +1300

    2024 Day 7 - Major overhaul
    Once I worked out that all the operations were easily inverted and verified, it became a lot faster. All the operators act as A op B = C, so these unoperators act as C unop B = A

--- 2024/07.py
+++ 2024/07.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 from time import perf_counter
-from operator import add, mul
 timer_script_start=perf_counter()
 SCRIPT_PATH=Path(os.path.realpath(__file__))
 INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
@@ -13,22 +12,34 @@ cals = [(int(a),list(map(int,b.split()))) for a,b in (line.split(": ") for line 
 
 timer_parse_end=timer_part1_start=perf_counter()
 ############################## PART 1 ##############################
-OPERATORS = [add, mul]
-def gen_vals(line,index=-1):
-    index %= len(line)
-    if index == 0:
-        yield line[index]
-    else:
-        for val in gen_vals(line,index-1):
-            for op in OPERATORS:
-                yield op(val, line[index])
+def un_mul(c,b):
+    a,r = divmod(c,b)
+    if not r:
+        return a
+    
+def un_add(c,b):
+    if c >= b:
+        return c - b
 
-p1 = sum(cal for cal, nums in cals if any(a==cal for a in gen_vals(nums)))
+def un_cat(c,b):
+    a,b2 = divmod(c,10**len(str(b)))
+    if b==b2:
+        return a
+
+UNOPERATORS = [un_add, un_mul]
+
+def validate(test,nums):
+    vals = {test}
+    for b in nums[:0:-1]:
+        vals = {unop(c,b) for c in vals for unop in UNOPERATORS if c is not None}
+    return nums[0] in vals
+
+p1 = sum(cal for cal, nums in cals if validate(cal,nums))
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-OPERATORS.append(lambda a,b:int(f"{a}{b}"))
-p2 = sum(cal for cal, nums in cals if any(a==cal for a in gen_vals(nums)))
+UNOPERATORS.append(un_cat)
+p2 = sum(cal for cal, nums in cals if validate(cal,nums))
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
 print(f"""Execution times (sec)
