21a650e466c9296b3bcbf0abfe9caec2cf884de7
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-08 19:13:48 +1300

    2024 Day 8 - Slightly more general solution
    For all known inputs, dx and dy are coprime, so no antinodes appear between the nodes for part 1 (at 1/3 and 2/3). The problem states that there are two antinodes for each pair of nodes, which implies that this case never occurs for given inputs. This assumption doesn't quite extend to part 2, since that only implies that the difference isn't a multiple of 3, not that they're coprime

--- 2024/08.py
+++ 2024/08.py
@@ -12,7 +12,7 @@ timer_parse_start=perf_counter()
 with open(INPUT_PATH) as file:
     lines = file.read().splitlines()
 all_locs = {complex(i,j):c for i,line in enumerate(lines) for j,c in enumerate(line)}
-timer_parse_end=timer_part1_start=perf_counter()
+timer_parse_end=perf_counter()
 ############################## PART 1 ##############################
 freq_locs = defaultdict(list)
 for loc, c in all_locs.items():
@@ -21,29 +21,23 @@ for loc, c in all_locs.items():
 
 in_range = set(all_locs)
 antinodes = set()
+actual_antinodes = set()
 for freq, locs in freq_locs.items():
     for a, b in combinations(locs, 2):
         delta = a-b
         antinodes.add(a+delta)
         antinodes.add(b-delta)
-p1 = len(antinodes&in_range)
-
-print("Part 1:",p1)
-timer_part1_end=timer_part2_start=perf_counter()
-############################## PART 2 ##############################
-actual_antinodes = set()
-for freq, locs in freq_locs.items():
-    for a, b in combinations(locs, 2):
-        delta = a-b
+        antinodes.add(a-delta/3)
+        antinodes.add(b+delta/3)
         delta/=gcd(int(delta.real),int(delta.imag))
-        for i in range(-60,60):
+        for i in range(~len(lines),len(lines)+2):
             actual_antinodes.add(a+i*delta)
+p1 = len(antinodes&in_range)
+print("Part 1:",p1)
+############################## PART 2 ##############################
 p2 = len(actual_antinodes&in_range)
-
 print("Part 2:",p2)
-timer_part2_end=timer_script_end=perf_counter()
+timer_script_end=perf_counter()
 print(f"""Execution times (sec)
 Parse: {timer_parse_end-timer_parse_start:3.3f}
-Part1: {timer_part1_end-timer_part1_start:3.3f}
-Part2: {timer_part2_end-timer_part2_start:3.3f}
 Total: {timer_script_end-timer_script_start:3.3f}""")
