066181694213d7ed67258d69d58773f7951b2384
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-10 01:26:39 +1300

    2024 Day 9 - Use probably just as (in)efficient an algorithm, but without the linked list

--- 2024/09.py
+++ 2024/09.py
@@ -1,20 +1,20 @@
 import os
 from pathlib import Path
 from time import perf_counter
+from itertools import zip_longest
 timer_script_start=perf_counter()
 SCRIPT_PATH=Path(os.path.realpath(__file__))
 INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
 timer_parse_start=perf_counter()
 ############################## PARSER ##############################
 with open(INPUT_PATH) as file:
-    lines = file.read().splitlines()
-data ,= lines
+    data = file.read().strip()
 # data = "2333133121414131402"
+nums = list(map(int,data))
 timer_parse_end=timer_part1_start=perf_counter()
 ############################## PART 1 ##############################
 file_system = []
-for i,c in enumerate(data):
-    n = int(c)
+for i,n in enumerate(nums):
     fid = i//2
     if i%2: # gap
         file_system.extend((None for _ in range(n)))
@@ -35,84 +35,29 @@ p1 = sum(a*b for a,b in enumerate(file_system))
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-class DLLNode:
-    def __init__(self, size, contents=None) -> None:
-        self.next = None
-        self.prev = None
-        self.size = size
-        self.contents = contents
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.size!r}, {self.contents!r})"
-    def replace(self, new_node):
-        assert new_node is not self
-        new_node.next = self.next
-        new_node.prev = self.prev
-        self.next = None
-        self.prev = None
-        if new_node.next is not None:
-            new_node.next.prev = new_node
-        if new_node.prev is not None:
-            new_node.prev.next = new_node
-    def insert_before(self, new_node):
-        assert new_node is not self
-        new_node.next = self
-        new_node.prev = self.prev
-        self.prev = new_node
-        if new_node.prev is not None:
-            new_node.prev.next = new_node
-         
-head = tail = None
-block_nodes = []
-for i,c in enumerate(data):
-    n = int(c)
-    fid = i//2
-    if i%2: # gap
-        new_node = DLLNode(n,None)
-    else:
-        new_node = DLLNode(n,fid)
-        block_nodes.append(new_node)
-    if n == 0:
-        continue
-    if head is None:
-        head = tail = new_node
-    else:
-        tail.next = new_node
-        new_node.prev = tail
-        tail = new_node
+fills = nums[::2]
+frees = nums[1::2]
 
-def print_dll():
-    curr = head
-    while curr is not None:
-        c = "." if curr.contents is None else str(curr.contents)
-        print(" ".join(c*curr.size),end="|")
-        curr = curr.next
-    print()
+running_space = 0
+fill_locs = {}
+free_locs = []
+for fid,(fill,free) in enumerate(zip_longest(fills,frees,fillvalue=0)):
+    fill_locs[fid] = (running_space, fill)
+    running_space += fill
+    free_locs.append((running_space,free))
+    running_space += free
 
-for node in reversed(block_nodes):
-    curr = head
-    while curr.contents is not None or curr.size < node.size:
-        if curr is node:
-            break
-        curr = curr.next
-    else:
-        new_node = DLLNode(node.size,None)
-        node.replace(new_node)
-        curr.size -= node.size
-        if curr.size == 0:
-            curr.replace(node)
+for fid in reversed(range(len(fill_locs))):
+    fill_index, fill_size = fill_locs[fid]
+    for i,(free_index,free_size) in enumerate(free_locs):
+        if free_index < fill_index:
+            if free_size >= fill_size:
+                fill_locs[fid] = (free_index, fill_size)
+                free_locs[i] = (free_index+fill_size, free_size-fill_size)
+                break
         else:
-            curr.insert_before(node)
-
-
-p2 = 0
-i = 0
-curr = head
-while curr is not None:
-    for _ in range(curr.size):
-        if curr.contents is not None:
-            p2 += curr.contents * i
-        i += 1
-    curr = curr.next
+            break
+p2 = sum(fid*sum(range(fi,fi+fs)) for fid,(fi,fs) in fill_locs.items())
 
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
