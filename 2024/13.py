2024d137877c86c2b5c72932f886e82115d3aa96
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-13 18:26:00 +1300

    2024 Day 13

---
+++ 2024/13.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+from time import perf_counter
+import re
+import numpy as np
+timer_script_start=perf_counter()
+SCRIPT_PATH=Path(os.path.realpath(__file__))
+INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
+DIRECTIONS = [(0,1),(1,0),(0,-1),(-1,0)]
+def add(*ps): return tuple(map(sum,zip(*ps)))
+timer_parse_start=perf_counter()
+############################## PARSER ##############################
+with open(INPUT_PATH) as file:
+    lines = file.read().splitlines()
+nums = [list(map(int,re.findall(r"\d+",line))) for line in lines]
+As = nums[::4]
+Bs = nums[1::4]
+Ps = nums[2::4]
+timer_parse_end=timer_part1_start=perf_counter()
+############################## PART 1 ##############################
+p1 = 0
+for a,b,p in zip(As,Bs,Ps):
+    mat = np.array([a,b]).T
+    na, nb = np.linalg.solve(mat,p)
+    ca,cb = ab = round(na),round(nb)
+    if list(mat@ab)==p and na >= 0 and nb >= 0:
+        p1 += 3*ca + cb
+print("Part 1:",p1) # not 129, 169, 18394
+timer_part1_end=timer_part2_start=perf_counter()
+############################## PART 2 ##############################
+p2 = 0
+for a,b,p in zip(As,Bs,Ps):
+    mat = np.array([a,b]).T
+    ax,ay = a
+    bx,by = b
+    p = (p[0]+10000000000000,p[1]+10000000000000)
+    x,y = p
+    # mat = [[ax,bx],[ay,by]]
+    # mat_inv = [[by,-bx],[-ay,ax]]
+    mat_div = ax*by-bx*ay
+    na = by*x-bx*y
+    nb = -ay*x+ax*y
+    if na%mat_div == 0 and nb%mat_div==0:
+        na//=mat_div
+        nb//=mat_div
+        if na >= 0 and nb >= 0:
+            p2 += 3*na + nb
+
+print("Part 2:",p2)
+timer_part2_end=timer_script_end=perf_counter()
+print(f"""Execution times (sec)
+Parse: {timer_parse_end-timer_parse_start:3.3f}
+Part1: {timer_part1_end-timer_part1_start:3.3f}
+Part2: {timer_part2_end-timer_part2_start:3.3f}
+Total: {timer_script_end-timer_script_start:3.3f}""")
