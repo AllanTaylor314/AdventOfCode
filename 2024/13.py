5ef6095e0d9be2ac696c7fb351d2125156a890b5
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-14 00:16:10 +1300

    2024 Day 13 - Merge parts

--- 2024/13.py
+++ 2024/13.py
@@ -12,46 +12,34 @@ timer_parse_start=perf_counter()
 with open(INPUT_PATH) as file:
     lines = file.read().splitlines()
 nums = [list(map(int,re.findall(r"\d+",line))) for line in lines]
-As = nums[::4]
+As = nums[0::4]
 Bs = nums[1::4]
 Ps = nums[2::4]
-timer_parse_end=timer_part1_start=perf_counter()
+timer_parse_end=perf_counter()
 ############################## PART 1 ##############################
-p1 = 0
-for a,b,p in zip(As,Bs,Ps):
-    ax,ay = a
-    bx,by = b
-    x,y = p
+p1 = p2 = 0
+for (ax,ay),(bx,by),(x,y) in zip(As,Bs,Ps):
     mat_div = ax*by-bx*ay
     na = by*x-bx*y
     nb = -ay*x+ax*y
     if na%mat_div == 0 and nb%mat_div==0:
-        na//=mat_div
-        nb//=mat_div
+        na //= mat_div
+        nb //= mat_div
         if na >= 0 and nb >= 0:
             p1 += 3*na + nb
-print("Part 1:",p1)
-timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-p2 = 0
-for a,b,p in zip(As,Bs,Ps):
-    ax,ay = a
-    bx,by = b
-    p = (p[0]+10000000000000,p[1]+10000000000000)
-    x,y = p
-    mat_div = ax*by-bx*ay
+    x += 10000000000000
+    y += 10000000000000
     na = by*x-bx*y
     nb = -ay*x+ax*y
     if na%mat_div == 0 and nb%mat_div==0:
-        na//=mat_div
-        nb//=mat_div
+        na //= mat_div
+        nb //= mat_div
         if na >= 0 and nb >= 0:
             p2 += 3*na + nb
-
+print("Part 1:",p1)
 print("Part 2:",p2)
-timer_part2_end=timer_script_end=perf_counter()
+timer_script_end=perf_counter()
 print(f"""Execution times (sec)
 Parse: {timer_parse_end-timer_parse_start:3.3f}
-Part1: {timer_part1_end-timer_part1_start:3.3f}
-Part2: {timer_part2_end-timer_part2_start:3.3f}
 Total: {timer_script_end-timer_script_start:3.3f}""")
