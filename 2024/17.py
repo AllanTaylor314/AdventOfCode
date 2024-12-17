50d3a169a474071ac7f8b7681358070f8dd381fc
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-17 21:58:28 +1300

    2024 Day 17 - Use z3 for part 2 (manual decompilation - not a general solution across inputs)

--- 2024/17.py
+++ 2024/17.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from time import perf_counter
 import re
+import z3
 timer_script_start=perf_counter()
 SCRIPT_PATH=Path(os.path.realpath(__file__))
 INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
@@ -61,12 +62,18 @@ p1 = run_program()
 print("Part 1:",p1) # not 0,5,1,2,5,0,2,3,1
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-# target = ",".join(map(str,Program))
-# for p2 in count(): # just a casual 10**14 iterations...
-#     print(p2,end="\r")
-#     if run_program(p2) == target:
-#         break
-# print("Part 2:",p2)
+s = z3.Solver()
+original_A = A = z3.BitVec("A",len(Program)*3+7)
+for target_out in Program:
+    C = A >> (A & 0b111 ^ 0b101)
+    B = A ^ 0b011 ^ C
+    s.add(B & 0b111 == target_out)
+    A >>= 3
+s.add(A==0)
+while s.check() == z3.sat:
+    p2 = s.model()[original_A].as_long()
+    s.add(original_A < p2)
+print("Part 2:", p2)
 timer_part2_end=timer_script_end=perf_counter()
 print(f"""Execution times (sec)
 Parse: {timer_parse_end-timer_parse_start:3.3f}
