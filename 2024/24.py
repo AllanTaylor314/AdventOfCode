dba094bdf282f5fed82905f088e8c7c9356e66d3
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-24 19:25:12 +1300

    2024 Day 24 Part 2 - kinda
    This will just raise an exception on the problematic line, then it's on you to figure it out, fix it in the input, and run it again so it can hit the next error

--- 2024/24.py
+++ 2024/24.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from time import perf_counter
+from functools import cache
 timer_script_start=perf_counter()
 SCRIPT_PATH=Path(os.path.realpath(__file__))
 INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
@@ -12,6 +13,7 @@ with open(INPUT_PATH) as file:
     lines = file.read().splitlines()
 mid = lines.index("")
 wires = {k:int(v) for k,v in (line.split(": ") for line in lines[:mid])}
+inputs = list(wires)
 rule_lines = lines[mid+1:]
 ops = {"AND":lambda a,b:a*b, "OR":lambda a,b:a or b, "XOR":lambda a,b:int(a!=b)}
 timer_parse_end=timer_part1_start=perf_counter()
@@ -40,7 +42,27 @@ p1 = int(z_vals,2)
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
+rev_rules = {tuple(v):k for k,v in rules.items()}
+rev_rules.update({tuple(reversed(v)):k for k,v in rules.items()})
+@cache
+def dependencies(rule):
+    if rule in inputs:
+        return set()
+    left, op, right = rules[rule]
+    return dependencies(left)|dependencies(right)|{left,right}
+
 p2 = 0
+i = 0
+sum_out = rev_rules[f"x{i:02d}","XOR",f"y{i:02d}"]
+carry_out = rev_rules[f"x{i:02d}","AND",f"y{i:02d}"]
+s_ins = rules[f"z{i:02d}"]
+for i in range(1,len(inputs)//2):
+    ha_out = rev_rules[f"x{i:02d}","XOR",f"y{i:02d}"]
+    hac_out = rev_rules[f"x{i:02d}","AND",f"y{i:02d}"]
+    hac2_out = rev_rules[ha_out,"AND",carry_out]
+    sum_out = rev_rules[ha_out,"XOR",carry_out]
+    carry_out = rev_rules[hac_out,"OR",hac2_out]
+    print(f"{i:02d}: ",sum_out, carry_out)
 
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
