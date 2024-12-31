7bf558e293343b9eb05897e9173586df6a4dfd93
Allan Taylor <allan.taylor.pi@gmail.com>
2024-12-31 16:52:20 +1300

    2024 Day 1 in OCaml - more maps and less recursion

--- 2024/01.ml
+++ 2024/01.ml
@@ -1,12 +1,11 @@
 open Printf
 
 let process_line line =
-  List.map int_of_string
-  @@ List.filter (fun s -> s <> "")
-  @@ String.split_on_char ' ' line
+  String.split_on_char ' ' line
+  |> List.filter (fun s -> s <> "")
+  |> List.map int_of_string
 
-let rec read_all =
- fun () ->
+let rec read_all () =
   match try read_line () |> process_line with _ -> [] with
   | [] -> []
   | a -> a :: read_all ()
@@ -21,30 +20,19 @@ let right_list =
   List.sort compare
   @@ List.map (fun pair -> match pair with _ :: a :: _ -> a | _ -> 0) values
 
-let rec part1 =
- fun left right ->
-  match left with
-  | top_left :: remaining_left -> (
-      match right with
-      | top_right :: remaining_right ->
-          abs (top_left - top_right) + part1 remaining_left remaining_right
-      | [] -> 0)
-  | [] -> 0
+let part1 left right =
+  List.combine left right
+  |> List.map (fun p -> abs (fst p - snd p))
+  |> List.fold_left ( + ) 0
 
 let () = printf "Part 1: %d\n" (part1 left_list right_list)
 
-let rec count_occurrence =
- fun needle haystack ->
-  match haystack with
-  | top :: remaining ->
-      (if top = needle then 1 else 0) + count_occurrence needle remaining
-  | [] -> 0
+let count_occurrence needle haystack =
+  List.map (fun item -> if needle = item then 1 else 0) haystack
+  |> List.fold_left ( + ) 0
 
-let rec part2 =
- fun left right ->
-  match left with
-  | top :: remaining ->
-      (top * count_occurrence top right) + part2 remaining right
-  | [] -> 0
+let part2 left right =
+  List.map (fun top -> top * count_occurrence top right) left
+  |> List.fold_left ( + ) 0
 
 let () = printf "Part 2: %d\n" (part2 left_list right_list)
