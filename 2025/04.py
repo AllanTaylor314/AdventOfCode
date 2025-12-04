2025d047670b1d2b9c68360ad14ae023e5944a3d
Allan Taylor <allan_taylor@trimble.com>
2025-12-04 18:06:26 +1300

    2025 Day 4

---
+++ 2025/04.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+from time import perf_counter
+timer_script_start=perf_counter()
+SCRIPT_PATH=Path(os.path.realpath(__file__))
+INPUT_PATH=SCRIPT_PATH.parent.parent/"inputs"/Path(SCRIPT_PATH.parent.name,SCRIPT_PATH.stem+".txt")
+DIRECTIONS = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]#[::2]
+def add(*ps): return tuple(map(sum,zip(*ps)))
+timer_parse_start=perf_counter()
+############################## PARSER ##############################
+with open(INPUT_PATH) as file:
+    lines = file.read().splitlines()
+papers = {(i,j) for i,row in enumerate(lines) for j,c in enumerate(row) if c == '@'}
+timer_parse_end=timer_part1_start=perf_counter()
+############################## PART 1 ##############################
+p1 = 0
+for paper in papers:
+    count = sum(add(paper,d) in papers for d in DIRECTIONS)
+    if count < 4:
+        p1 += 1
+print("Part 1:",p1)
+timer_part1_end=timer_part2_start=perf_counter()
+############################## PART 2 ##############################
+p2 = 0
+changed = True
+while changed:
+    changed = False
+    for paper in list(papers):
+        count = sum(add(paper,d) in papers for d in DIRECTIONS)
+        if count < 4:
+            papers.remove(paper)
+            p2 += 1
+            changed = True
+
+print("Part 2:",p2)
+timer_part2_end=timer_script_end=perf_counter()
+print(f"""Execution times (sec)
+Parse: {timer_parse_end-timer_parse_start:3.3f}
+Part1: {timer_part1_end-timer_part1_start:3.3f}
+Part2: {timer_part2_end-timer_part2_start:3.3f}
+Total: {timer_script_end-timer_script_start:3.3f}""")
