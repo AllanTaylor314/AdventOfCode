2025d0965270fe7b01368bf0b481c2490b5387aa
Allan Taylor <allan_taylor@trimble.com>
2025-12-09 22:57:45 +1300

    2025 Day 9 add plotting

--- 2025/09.py
+++ 2025/09.py
@@ -22,11 +22,11 @@ def area(ps):
 
 timer_parse_end=timer_part1_start=perf_counter()
 ############################## PART 1 ##############################
-p1 = max(map(area,combinations(coords, 2)))
+p1 = area(box1:=max(combinations(coords, 2), key=area))
 print("Part 1:",p1)
 timer_part1_end=timer_part2_start=perf_counter()
 ############################## PART 2 ##############################
-bounds = Polygon(coords).buffer(0.3)
+bounds = Polygon(coords).buffer(0.51, cap_style='square', join_style='mitre')
 options = []
 
 for a,b in combinations(coords, 2):
@@ -36,7 +36,7 @@ for a,b in combinations(coords, 2):
     if box.within(bounds):
         options.append((a,b))
 
-p2 = max(map(area,options))
+p2 = area(box2:=max(options, key=area))
 print("Part 2:",p2)
 timer_part2_end=timer_script_end=perf_counter()
 print(f"""Execution times (sec)
@@ -44,3 +44,14 @@ Parse: {timer_parse_end-timer_parse_start:3.3f}
 Part1: {timer_part1_end-timer_part1_start:3.3f}
 Part2: {timer_part2_end-timer_part2_start:3.3f}
 Total: {timer_script_end-timer_script_start:3.3f}""")
+
+if 0:
+    import matplotlib.pyplot as plt
+    from shapely.plotting import plot_polygon
+
+    plot_polygon(bounds,color='red')
+    (x1,y1),(x2,y2) = box1
+    plot_polygon(Polygon.from_bounds(min(x1,x2),min(y1,y2),max(x1,x2),max(y1,y2)).buffer(0.49, cap_style='square', join_style='mitre'),color='blue')
+    (x1,y1),(x2,y2) = box2
+    plot_polygon(Polygon.from_bounds(min(x1,x2),min(y1,y2),max(x1,x2),max(y1,y2)).buffer(0.49, cap_style='square', join_style='mitre'),color='green')
+    plt.show()
